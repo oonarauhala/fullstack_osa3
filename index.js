083ae18538f36075138871adcf161bef97153882
Oona Rauhala <oona.e.rauhala@gmail.com>
2022-02-04 14:07:20 +0200

    Add 3.8

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const { response } = require('express')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+morgan.token('reqbody', function (req) {
+    return JSON.stringify(req.body)
+})
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqbody'))
 
 let persons = {
     "persons": [
